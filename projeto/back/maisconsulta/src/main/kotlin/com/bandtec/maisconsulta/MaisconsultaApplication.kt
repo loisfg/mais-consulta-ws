fe939ab85383c154a0efde03ccac6b08587d6328
Gustavo Oliveira <gustavo.oliveira@valemobi.com.br>
2021-09-24 13:00:03 -0300

    feat(back): cria projeto base

---
+++ projeto/back/maisconsulta/src/main/kotlin/com/bandtec/maisconsulta/MaisconsultaApplication.kt
@@ -0,0 +1,11 @@
+package com.bandtec.maisconsulta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MaisconsultaApplication
+
+fun main(args: Array<String>) {
+	runApplication<MaisconsultaApplication>(*args)
+}
