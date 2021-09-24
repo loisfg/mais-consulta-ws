9c28659b9e41491836d382506534b2a0fee37b7b
Gustavo Oliveira <gustavo.oliveira@valemobi.com.br>
2021-09-24 13:14:20 -0300

    refactor

--- projeto/back/maisconsulta/src/main/kotlin/com/bandtec/maisconsulta/MaisConsultaApplication.kt
+++ projeto/back/maisconsulta/src/main/kotlin/com/bandtec/maisconsulta/MaisConsultaApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MaisconsultaApplication
+class MaisConsultaApplication
 
 fun main(args: Array<String>) {
-	runApplication<MaisconsultaApplication>(*args)
+	runApplication<MaisConsultaApplication>(*args)
 }
