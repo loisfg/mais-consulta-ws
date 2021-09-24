ee647f627c6d894c55edb04d87609d5aefe9c2ef
gustavo.oliveira <gustavo.oliveira@bandtec.com.br>
2021-09-24 20:43:41 -0300

    refactor(back): altera o projeto de kotlin para java

---
+++ projeto/back/mais-consulta/src/main/java/com/bandtec/mais/consulta/MaisConsultaApplication.java
@@ -0,0 +1,13 @@
+package com.bandtec.mais.consulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisConsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisConsultaApplication.class, args);
+	}
+
+}
