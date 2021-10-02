5ce1ffb683df24f7d8b0288d26d9697665c2923e
Thais Calazans <thaiscalazans02@gmail.com>
2021-10-02 15:18:53 -0300

    feat: Iniciar componetização da tela de login

---
+++ projeto/front/mais-consulta/src/components/FormLogin/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 40rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 40px;
+  justify-content: center;
+  img{
+      width: 30%;
+  }
+`;
