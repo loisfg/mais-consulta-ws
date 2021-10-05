206ad63e428e42520c4cab27b9354b6633b95cad
gustavo.oliveira <gustavo.oliveira@bandtec.com.br>
2021-10-05 16:45:37 -0300

    feat(back): adiciona crossOrigin

--- projeto/back/mais-consulta/src/main/java/com/bandtec/mais/consulta/controller/AuthController.java
+++ projeto/back/mais-consulta/src/main/java/com/bandtec/mais/consulta/controller/AuthController.java
@@ -9,15 +9,13 @@ import com.bandtec.mais.consulta.usecase.Signup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("auth")
 public class AuthController {
