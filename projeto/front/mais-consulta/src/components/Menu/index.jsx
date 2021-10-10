833f2f6978a0687bc8cb2335ac82642a9681f313
BeatrizCDS <beatriz.santos@bandtec.com.br>
2021-10-09 21:37:09 -0300

    feat(front): component menu

--- projeto/front/mais-consulta/src/components/Menu/index.jsx
+++ projeto/front/mais-consulta/src/components/Menu/index.jsx
@@ -1,29 +1,60 @@
 import * as React from 'react';
-import { Tab, DivMenu, DivTab } from './styles';
+import { Tab, DivMenu, DivTab, LinkDivTab } from './styles';
 import Logo from '../../assets/logo.svg';
+import Home from '../../assets/home-selected.png';
+import User from '../../assets/user.png';
+import History from '../../assets/historico.png';
+import Unidades from '../../assets/unidades.png';
+import Calendar from '../../assets/calendar.png';
+import Logoff from '../../assets/sair.png';
+import { style } from '@material-ui/system';
 export const Menu = () => {
   return(
     <DivMenu>
       <img src={Logo} alt="" />
       <DivTab>
-        <Tab>
-          Home
-        </Tab>
-        <Tab>
-          Histórico
-        </Tab>
-        <Tab>
-          Agendamento
-        </Tab>
-        <Tab>
-          Mapa de agendamento
-        </Tab>
-        <Tab>
-          Perfil
-        </Tab>
-        <Tab>
-          Sair
-        </Tab>
+        <LinkDivTab>
+          <img src={Home} alt="" />
+          <Tab>
+            Home
+          </Tab>  
+        </LinkDivTab>
+
+        <LinkDivTab>
+        <img src={History} alt="" />
+          <Tab>
+            Histórico
+          </Tab>  
+        </LinkDivTab>
+
+        <LinkDivTab>
+        <img src={Calendar} alt="" />
+          <Tab>
+            Agendamento
+          </Tab>  
+        </LinkDivTab>
+
+        <LinkDivTab>
+        <img src={Unidades} alt="" />
+          <Tab>
+            Mapa de agendamento
+          </Tab>  
+        </LinkDivTab>
+
+        <LinkDivTab>
+        <img src={User} alt="" />
+          <Tab>
+            Perfil
+          </Tab>  
+        </LinkDivTab>
+
+        <LinkDivTab>
+        <img src={Logoff} alt="" />
+          <Tab>
+            Sair
+          </Tab>  
+        </LinkDivTab>   
+
       </DivTab>
     </DivMenu>
   );
--- projeto/front/mais-consulta/src/components/Menu/styles.js
+++ projeto/front/mais-consulta/src/components/Menu/styles.js
@@ -10,8 +10,8 @@ export const DivMenu = styled.div`
   justify-content: space-between;
   align-items: flex-start;
   img {
-    height: 150px;
-    width: 150px;
+    height: 190px;
+    width: 190px;
     align-self: center;
     justify-self: flex-start;
   }
@@ -21,12 +21,35 @@ export const DivTab = styled.div`
   display: flex;
   flex-direction: column;
 `
-export const Tab = styled.button`
-  font-size: 13px;
-  border-style: none;
-  background-color: #fff;
-  text-align: start;
-  color: #19A795;
-  margin: 0 0 35px 27px;
-  font-weight: 600;
+export const LinkDivTab = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content:center;
+  aling-items:end;
+  img {
+    height: 3vh;
+    width: 3vh;
+  }
+  &:hover {
+    border-left: 5px solid #19A795;
+  }
 `
+
+export const Tab = styled.button`
+  font-size: 17px;
+  border-style: none;
+  background:white;
+  height:100%;
+  text-align: start;
+  color: #515151;
+  width: 70%;
+  margin: 0 0 35px 0px;
+  padding-left: 27px;
+  font-weight: 600;
+  cursor:pointer; 
+  
+  &:hover {
+    color: #19A795;
+  }
+` 
