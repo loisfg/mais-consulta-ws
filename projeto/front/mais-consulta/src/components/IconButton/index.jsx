925092258e509f1394c690d561be706d7e043aee
yodalis <thaiscalazans02@gmail.com>
2021-10-12 18:13:57 -0300

    refactor(front): colocar left arrow como padrao

--- projeto/front/mais-consulta/src/components/IconButton/index.jsx
+++ projeto/front/mais-consulta/src/components/IconButton/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { CustomIconButton } from './styles'
+import LeftArrow from '../../assets/left_arrow.svg';
 
-export const IconButton = ({onClick, Arrow}) => {
+export const IconButton = ({onClick, Arrow = LeftArrow}) => {
   return (
       <CustomIconButton onClick= {onClick}>
         <img src={Arrow} alt="" />
