a9c082f94d1bf918fbb3848908e50fc4aedebc9a
Thais Calazans <thaiscalazans02@gmail.com>
2021-10-16 14:29:25 -0300

    refactor(front): adicionar rotas ao menu

--- projeto/front/mais-consulta/src/routes.js
+++ projeto/front/mais-consulta/src/routes.js
@@ -1,35 +1,23 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import { isAuth } from "./services/auth";
-import { Initial } from "../src/pages/";
-import HomePatient from "./pages/HomePatient";
-import { Calendar } from "./components/Calendar";
-import { Profile } from './pages/Profile'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { Calendar, Menu } from "../src/components"
+import { HomePatient, Initial, Profile } from "../src/pages"
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      isAuth() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
-      )
-    }
-  />
-);
-
-const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Initial} />
-      <Route path="/home" component={HomePatient} />
-      <Route path="/calendar" component={Calendar} />
-      <Route path="/profile" component={Profile} />
-      <PrivateRoute path="/app" component={() => <h1> App </h1>} />
-      <Route path="*" component={() => <h1> Page Not Found </h1>} />
-    </Switch>
-  </BrowserRouter>
-);
+ const Routes =  () => {
+  return (
+    <Router>
+      <div className='wrapper'>
+          <Menu/>
+          <Switch>
+              <Route exact path="/" component={Initial} />
+              <Route path="/home" component={HomePatient} />
+              <Route path="/calendar" component={Calendar} />
+              <Route path="/perfil" component={Profile} />
+              <Route path="*" component={() => <h1> Page Not Found </h1>} />
+          </Switch>
+      </div>
+    </Router>
+  );
+}
 
 export default Routes;
