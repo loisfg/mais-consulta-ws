63ba53443401ef416647bc2c9886145bcb70d4a2
BeatrizCDS <beatriz.santos@bandtec.com.br>
2021-10-17 16:27:39 -0300

    feat(front) component maps

---
+++ projeto/front/mais-consulta/src/components/Maps/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Mapa } from './styles'
+
+export const Maps = () => {
+    return (
+        <Mapa>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14631.223224096546!2d-46.4551959!3d-23.539486!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8466a56349d5dd62!2sUBS%20Itaquera!5e0!3m2!1spt-BR!2sbr!4v1634497152085!5m2!1spt-BR!2sbr" style={{width:646, height:532, border:0}} allowfullscreen="" loading="lazy"></iframe>
+        </Mapa>
+    );
+}
+// allowfullscreen="" loading="lazy"
---
+++ projeto/front/mais-consulta/src/components/Maps/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Mapa = styled.div`
+    height: 90%;
+    width: 95%;
+    background: #e5e5e5;
+    border-radius: 3px;
+    border: 2px solid #e5e5e5;
+`
