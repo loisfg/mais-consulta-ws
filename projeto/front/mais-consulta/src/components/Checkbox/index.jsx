bd7fa6bb5335e8a25ebb9cbb871afdf78e5192ca
Gustavo Oliveira <gustavo.oliveira@valemobi.com.br>
2021-11-03 20:44:00 -0300

    feat(front): adiciona checkbox para login de medico

--- projeto/front/mais-consulta/src/components/Checkbox/index.jsx
+++ projeto/front/mais-consulta/src/components/Checkbox/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { CustomStyledCheckbox } from "./styles"
-import { FormControlLabel } from '@material-ui/core';
+import React from "react";
+import { CustomStyledCheckbox } from "./styles";
+import { FormControlLabel } from "@material-ui/core";
 
-export const Checkbox = ({ label }) =>
+export const Checkbox = ({ label, onClick, checked }) => (
   <FormControlLabel
     control={
       <CustomStyledCheckbox
         defaultChecked
-        sx={{ '& .MuiSvgIcon-root': { fontSize: '2.7rem', color: "#3C5D7C" } }}
+        sx={{ "& .MuiSvgIcon-root": { fontSize: "2.7rem", color: "#3C5D7C" } }}
+        onClick={onClick}
+        checked={checked}
       />
     }
     label={label}
   />
+);
--- projeto/front/mais-consulta/src/components/FormLogin/index.jsx
+++ projeto/front/mais-consulta/src/components/FormLogin/index.jsx
@@ -1,54 +1,59 @@
-import React, { useState, useCallback } from 'react';
-import { useHistory } from "react-router-dom"
-import { CustomForm, Div, CustomInputMask } from './styles';
-import { Button, Input, Checkbox } from '../'
-import api from "../../services/api"
-import { login } from '../../services/auth';
+import React, { useState, useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import { CustomForm, Div } from "./styles";
+import { Button, Input, Checkbox } from "../";
+import api from "../../services/api";
+import { login } from "../../services/auth";
 
 export const FormLogin = () => {
-  const history = useHistory()
-  const [cpf, setCpf] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
+  const history = useHistory();
+
+  const [cpf, setCpf] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const [isRemember, setIsRemember] = useState(true);
+  const [isMedico, setIsMedico] = useState(false);
 
   const handleLogin = useCallback(
     async (e) => {
-      e.preventDefault()
+      e.preventDefault();
       if (!cpf && !password) {
-        setError("Preencha cpf e password para continuar!")
+        setError("Preencha cpf e password para continuar!");
       } else {
         try {
           let params = {
             cpf: cpf.replace(".", "").replace(".", "").replace("-", ""),
-            password: password
-          }
+            password: password,
+          };
 
           console.log(params);
 
-          const response = await api.post("/auth/signin", params)
+          let response = {};
+
+          if (isMedico === false) {
+            response = await api.post("/auth/signin", params);
+          } else if (isMedico === true) {
+            response = await api.post("//signin", params);
+          }
+
           if (response.status === 200) {
-            const usuarioString = JSON.stringify(response.data)
+            const usuarioString = JSON.stringify(response.data);
             login(usuarioString);
-            history.push("/home")
+            history.push("/home");
           }
         } catch (erro) {
           console.log(erro);
-          setError("Cpf ou senha incorretos")
+          setError("Cpf ou senha incorretos");
         }
       }
-    }, [cpf, password, history])
+    },
+    [cpf, password, history, isMedico]
+  );
 
   return (
     <>
       <CustomForm onSubmit={handleLogin}>
-        {/* <CustomInputMask
-          id="cpf"
-          required
-          mask='999.999.999-99'
-          value={cpf}
-          onChange={(event) => setCpf(event.target.value)}
-        >
-        </CustomInputMask> */}
         <Input
           label="CPF"
           type="text"
@@ -56,7 +61,7 @@ export const FormLogin = () => {
           id="text"
           size="big"
           variant="standard"
-          onChange={e => setCpf(e.target.value)}
+          onChange={(e) => setCpf(e.target.value)}
           placeholder="000.000.000-00"
         />
         <Input
@@ -66,16 +71,27 @@ export const FormLogin = () => {
           id="password"
           size="big"
           variant="standard"
-          onChange={e => setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
         />
         {error && <p>{error}</p>}
         <Div>
-          <Checkbox label='Lembrar de mim' />
-          <Button type="submit" text='Entrar' />
+          <Checkbox
+            label="Lembrar de mim"
+            checked={isRemember}
+            onClick={(_) => {
+              setIsRemember(!isRemember);
+            }}
+          />
+          <Checkbox
+            label="Sou médico"
+            checked={isMedico}
+            onClick={(_) => {
+              setIsMedico(!isMedico);
+            }}
+          />
+          <Button type="submit" text="Entrar" />
         </Div>
       </CustomForm>
     </>
-  )
-
-
+  );
 };
