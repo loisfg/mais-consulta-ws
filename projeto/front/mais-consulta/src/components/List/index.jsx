412d534e512cdfe82ec7b4312a2051bdb614dbfe
BeatrizCDS <beatriz.santos@bandtec.com.br>
2021-11-04 18:40:57 -0300

    refactor(front) inicio integracao

--- projeto/front/mais-consulta/src/components/List/index.jsx
+++ projeto/front/mais-consulta/src/components/List/index.jsx
@@ -1,29 +1,46 @@
 import React from "react";
-import {  P, Lista, ListItem, H3, H2, H1 } from "./styles";
+import { P, Lista, ListItem, H3, H2, H1 } from "./styles";
 
-const list = [
-    {ubs: "UBS Vila Bartira", endereco: "Rua da saudade, 73", tel: "(11)98304-5357"},
-    {ubs: "UBS Jardim da alegria", endereco: "Rua João Santos, 27", tel: "(11)98304-5357"},
-    {ubs: "UBS Jardim da alegria", endereco: "Rua João Santos, 27", tel: "(11)98304-5357"},
-    {ubs: "UBS Jardim da alegria", endereco: "Rua João Santos, 27", tel: "(11)98304-5357"}
-];
+// const list = [
+//     {ubs: "UBS Vila Bartira", endereco: "Rua da saudade, 73", tel: "(11)98304-5357"},
+//     {ubs: "UBS Jardim da alegria", endereco: "Rua João Santos, 27", tel: "(11)98304-5357"},
+//     {ubs: "UBS Jardim da alegria", endereco: "Rua João Santos, 27", tel: "(11)98304-5357"},
+//     {ubs: "UBS Jardim da alegria", endereco: "Rua João Santos, 27", tel: "(11)98304-5357"}
+// ];
 
-const listItem = list.map((props) =>
-    <ListItem onClick={() => {
-        alert(props+"selecionado")
-    }}>
-        <H3>{props.ubs}</H3>
-        <H2>{props.endereco}</H2>
-        <H1>tel: {props.tel}</H1>
-    </ListItem>
-);
-export const List  = ({props, text}) => {
+// const listItem = list.map((props) =>
+//     <ListItem onClick={() => {
+//         alert(props + "selecionado")
+//     }}>
+//         <H3>{props.ubs}</H3>
+//         <H2>{props.endereco}</H2>
+//         <H1>tel: {props.tel}</H1>
+//     </ListItem>
+// );
+export const List = ({ text }) => {
 
-    return(
+    const [listUbs, setListUbs] = useState([]);
+
+    useEffect(() => {
+      async function searchListUbs() {
+        const resp = await api.get("")
+        setListUbs(resp.data)
+        console.log("log", resp.data);
+      }
+      searchListUbs();
+    },[]);
+  
+    return (
         <>
             <P>{text}</P>
             <Lista>
-            {listItem}
+                <ListItem onClick={() => {
+                    alert(listUbs + "selecionado")
+                }}>
+                    <H3>{listUbs.ubs}</H3>
+                    <H2>{listUbs.endereco}</H2>
+                    <H1>tel: {listUbs.tel}</H1>
+                </ListItem>
             </Lista>
         </>
     );
---
+++ projeto/front/mais-consulta/src/pages/Schedules/index.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { Page } from "./styles";
+import { DivUsuario, Content, BoxLeft, BoxRight, BoxAux } from "./styles";
+import { UserProfilePic, Message, Specialty, List, Calendar, Hours, Button } from "../../components";
+import api from "../../../src/services/api";
+import swal from 'sweetalert';
+ 
+export const Schedules = ({ usuario }) => {
+  
+  // const [listUbs, setListUbs] = useState([]);
+
+  // useEffect(() => {
+  //   async function searchListUbs() {
+  //     const resp = await api.get("")
+  //     setListUbs(resp.data)
+  //     console.log("log", resp.data);
+  //   }
+  //   searchListUbs();
+  // },[]);
+
+
+    // const alergia = [
+    //     {
+    //         nome: "formiga"
+    //     }
+    // ]
+    // async function schedule() {
+    //     requisicao("post","infos/alergia/","formiga")
+    // }
+    
+    return (
+
+        <Page>
+            <DivUsuario>
+                <UserProfilePic nome="Tha Calazans" subtexto=" Paciente" />
+            </DivUsuario>
+            <Content>
+                <BoxLeft>
+                    <Message textOne="Novo agendamento de consulta" textTwo="" />
+                    <Specialty nameSpecialty="Dermatologia" />
+                    <BoxAux>
+                        <List text="Escolha a unidade desejada" />
+                    </BoxAux>
+                </BoxLeft>
+                <BoxRight>
+                    <Calendar />
+                    <Hours />
+                    <button text="Agendar atendimento" onClick={()=> {
+                        swal({
+                            text: "Deseja finalizar seu agendamento?",
+                            buttons: true,
+                            closeOnClickOutside : false ,
+                            buttons: {
+                                cancel : 'Cancelar',
+                                confirm : {
+                                    text:'Confirmar',
+                                    className:'confirmar'
+                                },
+                                
+                            }, 
+                          })
+                          .then((agendar) => {
+                            if (agendar) {
+                              swal("Deseja baixar o documento de confirmação do agendamento?", {
+                                icon: "success",
+                              });
+                            } else {
+                              swal("Agendamento cancelado", {
+                                icon: "error",
+                              });
+                              
+                            }
+                          });
+                    }}
+                    >clica</button> 
+                </BoxRight>
+            </Content>
+       </Page>
+    );
+}
