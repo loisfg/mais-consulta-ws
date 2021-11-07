0d99592e493f2d4867cb6da90a8a9aded2d933aa
Marques <matheus.s.marques@accenture.com>
2021-11-07 14:41:51 -0300

    inicio tela de historico de agedamento

--- projeto/front/mais-consulta/src/pages/Patient/SchedulingHistory/index.jsx
+++ projeto/front/mais-consulta/src/pages/Patient/SchedulingHistory/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, DivUsuario, Mensage} from './styles';
+import { UserProfilePic} from '../../../components'
 
 // import { Container } from './styles';
 
-export const SchedulingHistory = () => {
+export const SchedulingHistory = ({ usuario }) => {
   return (
     <Container>
-
+        <DivUsuario>
+             <UserProfilePic nome={usuario.paciente.nome} subtexto="Paciente" />
+        </DivUsuario>
+           <Mensage>
+                Histórico de agendamentos
+           </Mensage>
     </Container>
     
   );
--- projeto/front/mais-consulta/src/pages/Patient/SchedulingHistory/styles.js
+++ projeto/front/mais-consulta/src/pages/Patient/SchedulingHistory/styles.js
@@ -1,4 +1,25 @@
+import { style } from "@material-ui/system";
 import styled from "styled-components";
 
 export const Container = styled.div`
+ height: 100vh;
+width: 100%;
+display: flex;
+justify-content: space-between;
+background-color: yellow;
+flex-direction: column;
+`
+
+
+export const DivUsuario = styled.div`
+height: 11vh;
+width: 100%;
+background-color: #fff;
+display: flex;
+justify-content: flex-end;
+background-color: red;
+`;
+
+export const Mensage = styled.div`
+display: flex;
 `
