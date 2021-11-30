cb7b88a0a7b04560811f62fe2ba5c0a27b224a42
gustavo.oliveira <gustavo.oliveira@bandtec.com.br>
2021-11-30 19:56:49 -0300

    refactor(back): personaliza campos dos exports csv

--- projeto/back/mais-consulta/src/main/java/com/bandtec/mais/consulta/usecase/export/impl/ExportConsultaImpl.java
+++ projeto/back/mais-consulta/src/main/java/com/bandtec/mais/consulta/usecase/export/impl/ExportConsultaImpl.java
@@ -3,6 +3,7 @@ package com.bandtec.mais.consulta.usecase.export.impl;
 import com.bandtec.mais.consulta.domain.Agendamento;
 import com.bandtec.mais.consulta.domain.Medico;
 import com.bandtec.mais.consulta.domain.Paciente;
+import com.bandtec.mais.consulta.domain.Ubs;
 import com.bandtec.mais.consulta.gateway.repository.AgendamentoRepository;
 import com.bandtec.mais.consulta.gateway.repository.UsuarioRepository;
 import com.bandtec.mais.consulta.usecase.export.ExportConsulta;
@@ -44,25 +45,26 @@ public class ExportConsultaImpl implements ExportConsulta {
     public String buildDadosArquivoAgendamento(List<Agendamento> agendamentoList) {
 
         StringBuilder texto = new StringBuilder();
-
+        String cabecalho = "data do atendimento;especialidade da consulta;nome do paciente;nome do medico;nome da ubs\n";
+        texto.append(cabecalho);
         agendamentoList.forEach(agendamento -> {
             String especialidade = agendamento.getEspecialidade().getDescricao();
             LocalDate dataAtendimento = agendamento.getDtAtendimento();
             Paciente paciente = agendamento.getPaciente();
             String nomePaciente = paciente.getNome();
-            String numeroCarteiraSus = paciente.getNumeroCarteiraSus();
-            String status = agendamento.getStatus();
             Medico medico = agendamento.getMedico();
             String nomeMedico = medico.getNome();
-            texto.append(buildTextoAgendamento(especialidade, dataAtendimento, nomePaciente, numeroCarteiraSus, status, nomeMedico));
+            Ubs ubs = medico.getUbs();
+            String nomeUbs = ubs.getNome();
+            texto.append(buildTextoAgendamento(especialidade, dataAtendimento, nomePaciente, nomeMedico, nomeUbs));
         });
 
         return texto.toString();
     }
 
     @NotNull
-    private String buildTextoAgendamento(String especialidade, LocalDate dataAtendimento, String nomePaciente, String numeroCarteiraSus, String status, String nomeMedico) {
-        return String.format("%s;%s;%s;%s;%s;%s\n", dataAtendimento, especialidade, nomePaciente, numeroCarteiraSus, status, nomeMedico);
+    private String buildTextoAgendamento(String especialidade, LocalDate dataAtendimento, String nomePaciente, String nomeMedico, String nomeUbs) {
+        return String.format("%s;%s;%s;%s;%s\n", dataAtendimento, especialidade, nomePaciente, nomeMedico, nomeUbs);
     }
 
 }
