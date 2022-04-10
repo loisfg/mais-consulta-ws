a7abbdc0e76634af4c6c27f8b8a0275ab95d8941
vinicius.s.oliveira <vinicius.s.oliveira@accenture.com>
2022-04-10 19:46:51 -0300

    Bugfix: Fixes the Post Hours Clinics request

--- src/main/java/com/bandtec/mais/consulta/usecase/clinic/PostHoursClinic.java
+++ src/main/java/com/bandtec/mais/consulta/usecase/clinic/PostHoursClinic.java
@@ -3,7 +3,8 @@ package com.bandtec.mais.consulta.usecase.clinic;
 
 import java.time.LocalTime;
 import java.util.HashMap;
+import java.util.List;
 
 public interface PostHoursClinic {
-    HashMap<LocalTime, String> execute(Integer clinicId, String day);
+    HashMap<String, List<LocalTime>> execute(Integer clinicId, String day);
 }
